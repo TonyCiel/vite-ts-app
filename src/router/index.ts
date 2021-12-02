7923e6e4498cb9c0f7d2d57230b5875747919b51
tangling <lizongliang@feinno.com>
2021-12-02 16:24:14 +0800

    增加loading

--- src/router/index.ts
+++ src/router/index.ts
@@ -52,14 +52,14 @@ const route = createRouter({
     routes: [...routes,...formatRoute(getStore("menu"))]
 })
 route.beforeEach((to, form) => {
-    // let loading = ElLoading.service({
-    //     lock: true,
-    //     text: 'Loading',
-    //     background: 'rgba(0, 0, 0, 0.7)',
-    // });
-    // setTimeout(() => {
-    //     loading.close()
-    // }, 200)
+    let loading = ElLoading.service({
+        lock: true,
+        text: 'Loading',
+        background: 'rgba(0, 0, 0, 0.7)',
+    });
+    setTimeout(() => {
+        loading.close()
+    }, 200)
     document.title = to.meta.title || to.name;
     // console.log(to,form);
 })
